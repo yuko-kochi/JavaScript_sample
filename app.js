ba37df70d0594019869528e0b00b6ab7ac27bf78
YukoKochi <happy.k165131@gmail.com>
2021-06-25 13:43:00 +0900

    [Start]1

---
+++ app.js
@@ -0,0 +1,15 @@
+var hello = 'Hello World'
+alert(hello);
+
+// 整数を代入する
+var int1 = 1;
+alert(int1);
+// 負数を代入する
+var int2 = -10;
+alert(int2);
+// 小数点を代入する
+var float1 = 3.14;
+alert(float1);
+// 文字列を代入する
+var str1 = 'JavaScriptを覚えよう';
+alert(str1);
